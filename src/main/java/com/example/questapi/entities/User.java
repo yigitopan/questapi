7f06d20b4fea9807d0b028f3df8ef4d5716e023b
yigit.opan <yigit.opan@mdpgroup.com>
2024-05-06 13:02:29 +0300

    first commit and JPA entity connection

---
+++ src/main/java/com/example/questapi/entities/User.java
@@ -0,0 +1,16 @@
+package com.example.questapi.entities;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+@Entity
+@Table(name = "users")
+@Data
+public class User {
+    @Id
+    Long id;
+    String username;
+    String password;
+}
---
+++ src/test/java/com/example/questapi/QuestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.questapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuestApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
